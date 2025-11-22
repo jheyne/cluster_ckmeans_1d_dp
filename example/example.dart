0da0ca01bf774a9241b7e332219c6ab976cc4f34
James Heyne <jheyne@gmail.com>
2025-11-21 23:59:15 -0800

    example and READ.ME

--- example/example.dart
+++ example/example.dart
@@ -2,11 +2,17 @@ import 'package:cluster_ckmeans_1d_dp/cluster_ckmeans_1d_dp.dart';
 
 void main(List<String> args) {
   final data = [1.0, 2.0, 3.0, 10.0, 11.0, 12.0];
-  final result = ckmeans(data, 2);
+  final result = findClusters(data, 2);
+
   assert(result.clusters.length == 2);
-  assert(result.clusters[0] == [1.0, 2.0, 3.0]);
-  assert(result.clusters[1] == [10.0, 11.0, 12.0]);
-  assert(result.centers[0] == 2.0);
-  assert(result.centers[1] == 11.0);
-  assert(result.sizes == [3.0, 3.0]);
+  assert(result.clusters[0].values == [1.0, 2.0, 3.0]);
+  assert(result.clusters[1].values == [10.0, 11.0, 12.0]);
+  assert(result.clusters[0].mean == 2.0);
+  assert(result.clusters[1].mean == 11.0);
+  assert(result.clusters[0].elementCount == 3.0);
+  assert(result.clusters[1].elementCount == 3.0);
+  assert(result.clusters[0].sumOfSquares == 2.0);
+  // (1-2)^2 + (2-2)^2 + (3-2)^2 = 1 + 0 + 1 = 2
+  assert(result.clusters[1].sumOfSquares == 2.0);
+  assert(result.totalSumOfSquares == 4.0);
 }
